72f54e6589c2b2d29147d4ae2db4254bef1447a1
zalqarnain-oddtech <zalqarnain@oddtechnologies.com>
2025-05-02 19:19:51 +0500

    coturn

--- public/client.js
+++ public/client.js
@@ -28,7 +28,7 @@ const iceServers = {
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
     {
-      urls: 'turn:visitor.supportsuite247.com:3478',
+      urls: 'turn:visitor.supportsuite247.com:3478', //coturn of ubuntu server
       username: 'webrtc',
       credential: '123456'
     },
